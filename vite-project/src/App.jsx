e35dd6838a84be322339eeffe5d6207cd747428a
Mikaeel92 <mikaeelyaghoobzadeh@gmail.com>
2024-12-31 09:26:32 +0330

    step one
    state managing

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,6 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const App = () => {
+
+  const [products, setProducts] = useState([])
+  const [count, setCount] = useState(0)
+  const [loading, setLoading] = useState(false)
+  const [errorMsg, setErrorMsg] = useState(null)
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch('')
+    } catch (error) {
+
+    }
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  if(errorMsg !== null) {
+    return <div>Error, {errorMsg} Data Is Loading... Please Wait</div>
+  }
+
+  if(loading) {
+    return <div> Data Is Loading... Please Wait</div>
+  }
+
   return (
     <div className='text-green-700 text-3xl'>App</div>
   )
